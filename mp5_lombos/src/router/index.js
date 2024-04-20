a4fc7b20639d8a3ba8fdfbaf07e4a6f7a681cabb
PrincessShu <ysabellaagbanglo14@gmail.com>
2024-04-20 09:53:07 +0800

    feat: create mp5

---
+++ mp5_lombos/src/router/index.js
@@ -0,0 +1,25 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomeView,
+    },
+    {
+      path: "/about",
+      name: "about",
+      component: () => import("../views/AboutView.vue"),
+    },
+    {
+      path: "/person/view",
+      name: "person",
+      component: () => import("../views/person/view.vue"),
+    },
+  ],
+});
+
+export default router;
